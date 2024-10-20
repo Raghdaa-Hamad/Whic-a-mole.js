3fc7e654e3b2b74c3eaa0017caf36c01542432c6
raghdaa-hamad <raghdaa.hamad24@gmail.com>
2024-10-20 21:51:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time_left");
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+
+let result = 0;
+let hitPosition;
+let currentTime = 10;
+let timeId = null;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+  hitPosition = randomSquare.id;
+}
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+function moveMole() {
+  timeId = setInterval(randomSquare, 1000);
+}
+moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timeId);
+    alert(`Game Over !!! Your final score is ${result}`);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
